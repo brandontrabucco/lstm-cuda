9ccce8bb2c97dfcfb879083da0657dd0fa37645f
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-10-29 21:46:56 +0800

    enable build with cuda

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,12 +9,15 @@ baseDir=$(cd `dirname "$0"`;pwd)
 
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
-cd $baseDir/..
+cd $baseDir/../src
 nvcc  -std=c++11 -L/usr/local/cuda/lib64 -lcuda -lcudart -dc *.cu
 if [ ! -d $baseDir/../build ]; then
+    rm -rf $baseDir/../build
     mkdir $baseDir/../build
 fi
-mv *.o $baseDir/../build
+tar cf - .|(cd $baseDir/../build; tar xf -) 
 cd $baseDir/../build
 nvcc *.o -o lstm
-./lstm
+echo "path:" $baseDir/../build/lstm
+# <learning rate> <decay rate> <blocks> <cells> <size ...>
+./lstm 0.01 0.01 10 100 10
